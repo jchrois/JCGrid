c7fe3c254fcdda5c2003111e98f8debfc964c853
Jakob C <jakob.chrois@gmail.com>
2014-12-18 23:56:55 +0100

    1

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
-index file testiclezsz
+<?php
+include("./db_class.php");
 
-<?php echo "TEST"; ?>
+
+
+
+
+
+
+try    {
+    /*** query the database ***/
+    $db = DB::getInstance();
+    $result = $db->query("SELECT * FROM animals");
+
+    /*** loop over the results ***/
+    foreach($result as $row)
+        {
+        print $row['animal_type'] .' - '. $row['animal_name'] . '<br />';
+        }
+    }
+catch(PDOException $e)
+    {
+    	echo $e->getMessage();
+    }
+
+
+?>
