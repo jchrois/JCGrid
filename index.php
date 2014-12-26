9d834c10561858f21a829ca82d16c3f1f81b4716
Jakob C <jakob.chrois@gmail.com>
2014-12-26 01:06:16 +0100

    baseline grid and modular type work

--- index.php
+++ index.php
@@ -2,12 +2,46 @@
 
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <title>JCGrid</title>
-  <link rel="stylesheet" href="./css/styles.css?v=1.0">
+  	<meta charset="utf-8">
+ 	<title>JCGrid</title>
+
+  	<script src="//use.typekit.net/zzt4aer.js"></script>
+	<script>try{Typekit.load();}catch(e){}</script>
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.15.0/plugins/CSSPlugin.min.js"></script>
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.15.0/easing/EasePack.min.js"></script>
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.15.0/TweenLite.min.js"></script>
+
+  	<link rel="stylesheet" href="./css/styles.css?v=1.0">
 </head>
 
 <body>
+
+	<div id="txt">
+		<h2>Thin air</h2>
+		<p>
+			Penguins (order Sphenisciformes, family Spheniscidae) are a group of aquatic,
+			flightless birds living almost exclusively in the Southern Hemisphere, especially in Antarctica. 
+			Highly adapted for life in the water, penguins have countershaded dark and white plumage, 
+			and their wings have evolved into flippers. Most penguins feed on krill, fish, 
+			squid and other forms of sealife caught while swimming underwater.
+			They spend about half of their lives on land and half in the oceans.
+		</p>
+
+		<small>Small text example here<br/><br/></small>
+
+		<p>
+			Bats are mammals of the order Chiroptera (/kaɪˈrɒptərə/; from the Greek χείρ - cheir, "hand"[2] and πτερόν 
+			- pteron, "wing"[3]) whose forelimbs form webbed wings, making them the only mammals naturally capable 
+			of true and sustained flight. By contrast, other mammals said to fly, such as flying squirrels,
+			gliding possums, and colugos, can only glide for short distances. Bats do not flap their entire forelimbs, 
+			as birds do, but instead flap their spread-out digits,[4] which are very long and covered with a thin membrane or patagium.
+		</p>
+
+		<div class="image">
+			<img src="http://www.mountainsoftravelphotos.com/Gauri%20Shankar%20and%20Menlungtse/Best/slides/Rolwaling%2098%20Gauri%20Shankar%20From%20Mountain%20Flight%202000.jpg">
+		</div>
+	</div>
+
 	<script src="./js/THREE/three.min.js"></script>
 	<script src="./js/JCGUI.js"></script>
 	<script src="./js/JCGrid_main.js"></script>
--- js/JCGrid_main.js
+++ js/JCGrid_main.js
@@ -3,10 +3,24 @@
 /*
 TODO
 
-GUI Baseline grid
+Combine GUI & Baseline grid
 Element snap to grid
 
 */
+
+
+//var col_width = 190;
+var grid_ratio = 1.618; //1.618;
+
+var grid_size_x;
+var grid_size_y;
+
+var column_amount = 16;
+
+var type_base;
+var type_scale = [];
+
+var grid_ctx;
 
 
 
@@ -17,7 +31,9 @@ window.onload = setup();
 var ww;
 var wh;
 
-var on3D;
+var on3D = false;
+var onGUI = false;
+
 var scene, camera, renderer;
 var light;
 
@@ -29,10 +45,13 @@ function setup() {
 	ww = window.innerWidth;
 	wh = window.innerHeight;
 
+	type_base = window.getComputedStyle(document.body, null).fontSize.slice(0, -2);
+
 	window.addEventListener('resize', onWindowResize, false);
 
-	create3D();
-	createGUI();
+	if(on3D) create3D();
+	if(onGUI) createGUI();
+	createGrid();
 	render();
 
 }
@@ -40,9 +59,9 @@ function setup() {
 
 function render() {
 	requestAnimationFrame(render);
-	if(on3D) {
-		renderer.render(scene, camera);	
-	}
+	
+	//updateGrid();
+	if(on3D) renderer.render(scene, camera);	
 	
 }
 
@@ -65,6 +84,120 @@ function create3D() {
 	on3D = true;
 
 }
+
+
+
+
+
+
+
+
+function createGrid() {
+
+	//console.log("Create grid");
+
+	var c = document.createElement("canvas");
+	c.id = "grid_canvas";
+	document.body.appendChild(c);
+	grid_ctx = c.getContext("2d");
+
+	type_scale = calcScale(type_base, grid_ratio);
+
+	updateGrid();
+	updateType();
+
+
+	var txt_div = document.getElementById('txt');
+	txt_div.isOpen = true;
+	txt_div.addEventListener("click", onTxtDivClick)
+	
+}
+
+
+
+function updateGrid() {
+
+
+	/* canvas draw */
+	grid_ctx.canvas.width = ww;
+	grid_ctx.canvas.height = wh;
+	
+	grid_size_x = ww /column_amount;
+	grid_size_y = Math.floor(type_scale[5]);
+
+
+	/* draw grid lines --------------------------------- */
+	var hor_lines = Math.floor((wh/type_base));
+	var ver_lines = Math.floor(ww/column_amount);
+
+	for(var i=0; i<hor_lines; ++i) {
+		
+		var y = (Math.floor((grid_size_y * (i+1)-grid_size_y))-0.5);
+		grid_ctx.beginPath();
+		grid_ctx.moveTo(0,y);
+		grid_ctx.lineTo(ww, y);
+		grid_ctx.lineWidth = 0.5;
+		grid_ctx.strokeStyle = (i%2) ? "#555555" : "#888888";
+		grid_ctx.stroke();
+		grid_ctx.closePath();
+		
+	}
+
+	for(var i=0; i<ver_lines; ++i) {
+		var x = (Math.floor(grid_size_x * (i+1))-0.5);
+		grid_ctx.beginPath();
+		grid_ctx.moveTo(x,0);
+		grid_ctx.lineTo(x, wh);
+		grid_ctx.lineWidth = 1;
+		grid_ctx.strokeStyle = '#33cccc';
+		grid_ctx.stroke();
+		grid_ctx.closePath();
+
+	}
+
+	var txt_div = document.getElementById('txt');
+	txt_div.style.width = grid_size_x * 10 + "px";
+	txt_div.style.height = grid_size_y * 35 + "px";
+	txt_div.style.top = grid_size_y * 2 + "px";
+	txt_div.style.left = grid_size_x * 1 + "px";
+
+
+}
+
+
+
+
+
+function updateType() {
+
+	updateTypeElement("p", type_base);
+	updateTypeElement("h1", type_scale[7], true);
+	updateTypeElement("h2", type_scale[6], true);
+	updateTypeElement("h3", type_scale[5], true);
+	updateTypeElement("h4", type_scale[4], true);
+	updateTypeElement("small", type_scale[3]);
+
+	function updateTypeElement(element, scale, head) {
+		var elements = document.querySelectorAll(element);
+		for(var i=0; i<elements.length;++i) {
+
+			var new_line_height = (head) ? grid_size_y*Math.ceil(scale/(grid_size_y)) : grid_size_y;
+			var margin_top = (head) ? grid_size_y : 0;
+
+			elements[i].style.fontSize = scale + "px";
+			elements[i].style.lineHeight = new_line_height + "px";
+			elements[i].style.marginTop = margin_top + "px";
+			elements[i].style.marginBottom = grid_size_y + "px";
+			elements[i].style.paddingLeft = grid_size_y + "px";
+			elements[i].style.paddingRight = grid_size_y + "px";
+		}
+
+	}
+
+}
+
+
+
 
 
 
@@ -103,7 +236,6 @@ function resizeGUI() {
 	inventory.width = 300;
 	inventory.height = 300;
 
-	
 	inventory.setXRel(0.05);
 	inventory.setYRel(0.70, true);
 	inventory_bnt1.setXRel(0.5, true);
@@ -120,12 +252,75 @@ function onWindowResize(){
 	ww = window.innerWidth;
 	wh = window.innerHeight;
 
-	resizeGUI();
+	updateGrid();
+
+	if(onGUI) resizeGUI();
 	
 	if(on3D) { 
 	    camera.aspect = window.innerWidth / window.innerHeight;
 	    camera.updateProjectionMatrix();
 	    renderer.setSize( window.innerWidth, window.innerHeight );   
+	}
+
+
+
+}
+
+
+
+
+
+function onTxtDivClick(e) {
+	//console.log(e.currentTarget.isOpen);
+	var isOpen = e.currentTarget.isOpen;
+	if(isOpen !== 'undefined' && isOpen) {
+		var ani = new TweenLite.to(e.currentTarget, 1, {css:{height:grid_size_y*3}, ease:Cubic.easeInOut});
+		e.currentTarget.isOpen = false;
+
+		fuckupText(e.currentTarget);
+
+	} else {
+		var ani = new TweenLite.to(e.currentTarget, 1, {css:{height:grid_size_y*35}, ease:Cubic.easeInOut});
+		e.currentTarget.isOpen = true;
+		restoreText(e.currentTarget);
+	}
+}
+
+
+
+function fuckupText(element) {
+	console.log("fuck up");
+	var nodes = element.childNodes;
+
+	for(var i=0; i<nodes.length; i++) {
+		//console.log(nodes[i].nodeName);
+		if(nodes[i].nodeName === 'H2') {
+			console.log("test");
+			TweenLite.to(nodes[i], 1, {css:{fontSize: type_base + "px", lineHeight: grid_size_y + "px"}, ease:Cubic.easeInOut});
+		}  else if(nodes[i].nodeName !== '#text') {
+	   	 	//console.log(nodes[i]);
+	   		//nodes[i].style.whiteSpace = "";
+	   		TweenLite.to(nodes[i], 1, {css:{color:"#ffffff"}, ease:Cubic.easeInOut});
+	   }
+	}
+
+}
+
+function restoreText(element) {
+	
+	var nodes = element.childNodes;
+
+	//element.style.background = "";
+
+	for(var i=0; i<nodes.length; i++) {
+	   if(nodes[i].nodeName === 'H2') {
+			console.log("test");
+			TweenLite.to(nodes[i], 1, {css:{fontSize: type_scale[6] + "px", lineHeight: grid_size_y*2 + "px"}, ease:Cubic.easeInOut});
+		} else if(nodes[i].nodeName !== '#text') {
+	   	 	//console.log(nodes[i]);
+	   		//nodes[i].style.whiteSpace = "";
+	   		TweenLite.to(nodes[i], 1, {css:{color:"#999999"}, ease:Cubic.easeInOut});
+	   }
 	}
 
 }
@@ -135,3 +330,34 @@ function onWindowResize(){
 
 
 
+
+
+
+
+/* Utils --------------------------------------------------------- */
+
+function calcScale(base, ratio) {
+	var scale = [];
+
+	scale.push(base);
+	scale.push((base/ratio).toFixed(2), ((base/ratio)/ratio).toFixed(2), (((base/ratio)/ratio)/ratio).toFixed(2), ((((base/ratio)/ratio)/ratio)/ratio).toFixed(2));
+	scale.push((base*ratio).toFixed(2), ((base*ratio)*ratio).toFixed(2), (((base*ratio)*ratio)*ratio).toFixed(2), ((((base*ratio)*ratio)*ratio)*ratio).toFixed(2));
+	scale.sort(function(a, b){return a-b});
+
+	return scale;
+
+}
+
+
+String.prototype.shuffle = function () {
+    var a = this.split(""),
+        n = a.length;
+
+    for(var i = n - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+    return a.join("");
+}
